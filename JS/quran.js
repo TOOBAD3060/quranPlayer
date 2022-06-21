457c7d77eb2a32c4132913bf26883eac33756666
TOOBAD <babatundetoheeb@outlook.com>
2022-06-21 22:56:57 +0100

    another commit

---
+++ JS/quran.js
@@ -0,0 +1,62 @@
+const allMusics=[
+    {
+        name:"Suratul Al-Fatihah",
+        artist:"Mishary Alafasi",
+        src:"/quran/001.mp3",
+        img:"/images/user-1.png"
+    },
+    {
+        name:"Suratul Nuh",
+        artist:"Abdulbasit Abdulsamad",
+        src:"/quran/071.mp3",
+        img:"/images/user-2.png"
+    },
+    {
+        name:"Suratul Ad-Duha",
+        artist:"Abdulbasit Abdulsamad",
+        src:"/quran/093.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul Quraish",
+        artist:"Mishary Alafasi",
+        src:"/quran/106.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul Al-Kafirun",
+        artist:"Mishary Alafasi",
+        src:"/quran/109.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul An-Nasr",
+        artist:"Mishary Alafasi",
+        src:"/quran/110.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul Al-Masad",
+        artist:"Mishary Alafasi",
+        src:"/quran/111.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul Al-Ikhlas",
+        artist:"Mishary Alafasi",
+        src:"/quran/112.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul An-Falaq",
+        artist:"Mishary Alafasi",
+        src:"/quran/113.mp3",
+        img:"/images/user-3.png"
+    },
+    {
+        name:"Suratul An-Nas",
+        artist:"Abdulbasit Abdulsamad",
+        src:"/quran/114.mp3",
+        img:"/images/user-3.png"
+    },
+]
