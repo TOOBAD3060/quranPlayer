766d1e64e99400d9f10342db90503ab7c8a86394
TOOBAD <babatundetoheeb@outlook.com>
2022-06-23 12:05:44 +0100

    successful

--- JS/quran.js
+++ JS/quran.js
@@ -9,54 +9,54 @@ const allMusics=[
         name:"Suratul Nuh",
         artist:"AbdulbasitAbdulsamad",
         src:"/quran/071.mp3",
-        img:"/images/user-2.png"
+        img:"/images/correctedTwo.jpg"
     },
     {
         name:"Suratul Ad-Duha",
         artist:"AbdulbasitAbdulsamad",
         src:"/quran/093.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedThree.jpg"
     },
     {
         name:"Suratul Quraish",
         artist:"MisharyAlafasi",
         src:"/quran/106.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedOne.jpg"
     },
     {
         name:"Suratul Al-Kafirun",
         artist:"MisharyAlafasi",
         src:"/quran/109.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedTwo.jpg"
     },
     {
         name:"Suratul An-Nasr",
         artist:"MisharyAlafasi",
         src:"/quran/110.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedThree.jpg"
     },
     {
         name:"Suratul Al-Masad",
         artist:"MisharyAlafasi",
         src:"/quran/111.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedOne.jpg"
     },
     {
         name:"Suratul Al-Ikhlas",
         artist:"MisharyAlafasi",
         src:"/quran/112.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedTwo.jpg"
     },
     {
         name:"Suratul An-Falaq",
         artist:"MisharyAlafasi",
         src:"/quran/113.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedThree.jpg"
     },
     {
         name:"Suratul An-Nas",
         artist:"AbdulbasitAbdulsamad",
         src:"/quran/114.mp3",
-        img:"/images/user-3.png"
+        img:"/images/correctedTwo.jpg"
     },
 ]
